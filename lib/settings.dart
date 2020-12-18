8f927b6751e25c9a64c36f81c2f449b0b68a9998
Christoph Stelz <mail@ch-st.de>
2020-12-18 10:22:03 +0100

    Add eSense selector

--- lib/settings.dart
+++ lib/settings.dart
@@ -5,6 +5,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'dart:async';
 
+const String CONFIG_ESENSE_NAME = "esenseName";
+
 class SettingsPage extends StatefulWidget {
   @override
   State<SettingsPage> createState() => SettingsState();
@@ -13,11 +15,13 @@ class SettingsPage extends StatefulWidget {
 class SettingsState extends State<SettingsPage> {
   String esenseName = "Kein Gerät ausgewählt";
   bool esenseSelected = false;
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
     SharedPreferences.getInstance().then((prefs) {
-      var v = prefs.getString("esenseName");
+      var v = prefs.getString(CONFIG_ESENSE_NAME);
       if (v == null) return;
       setState(() {
         esenseName = v;
@@ -26,12 +30,34 @@ class SettingsState extends State<SettingsPage> {
     });
   }
 
-  void selectEsense(ctx) {
-    showDialog(
+  void selectEsense(ctx) async {
+    if (!(await flutterBlue.isOn)) {
+      final errorSnackBar = SnackBar(
+          content: Row(children: [
+        Padding(
+            child: Icon(Icons.error, color: Colors.white),
+            padding: const EdgeInsets.only(right: 15)),
+        Text("Bitte Bluetooth aktivieren"),
+      ]));
+      _scaffoldKey.currentState.showSnackBar(errorSnackBar);
+      return;
+    }
+    var deviceName = await showDialog(
         context: ctx,
         builder: (BuildContext ctx) {
           return BlueDeviceSelector();
         });
+
+    print("Selected device $deviceName");
+    if (deviceName != null) {
+      SharedPreferences.getInstance().then((prefs) {
+        prefs.setString(CONFIG_ESENSE_NAME, deviceName);
+      });
+
+      setState(() {
+        esenseName = deviceName;
+      });
+    }
   }
 
   void getEsenseInfo() {}
@@ -43,21 +69,13 @@ class SettingsState extends State<SettingsPage> {
       print('Connection event: $event');
     });
 
-    FlutterBlue flutterBlue = FlutterBlue.instance;
-
-    flutterBlue.startScan(timeout: Duration(seconds: 5));
-    var sub = flutterBlue.scanResults.listen((results) {
-      for (ScanResult r in results) {
-        print('${r.device.name} found, rssi: ${r.rssi}, ${r.device}');
-      }
-    });
-
     return Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
           title: Text("Einstellungen"),
         ),
         body: Column(children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
             Text("eSense"),
             ElevatedButton(
               child: Text(esenseName),
